bf381cdf1f3076d6293d5e81ec6b282013930790
shital bramhe <shitalbramhe@gmail.com>
2022-02-04 17:35:26 +0530

    [Shital] add : Check employee is present or absent

---
+++ EmployeeWageComputationUsingPython.py
@@ -0,0 +1,15 @@
+'''
+@Author: Shital Bajait
+@Date: 2022-02-04 17:33:00
+@Last Modified by: Shital Bajait
+@Last Modified time: 2022-02-04 17:33:00
+@Title : Check employee is present or absent
+'''
+
+import random
+
+number = random.randint(0,1)
+if number == 1:
+    print("Employee is present")
+else:
+    print("Employee is absent")
