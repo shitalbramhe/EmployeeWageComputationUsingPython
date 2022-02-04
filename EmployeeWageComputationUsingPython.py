0d4a864259a6c1e7926fbeb559e1f8e2a5265fc0
shital bramhe <shitalbramhe@gmail.com>
2022-02-04 22:50:32 +0530

    [Shital] add : Calculate Daily Employee wage

--- EmployeeWageComputationUsingPython.py
+++ EmployeeWageComputationUsingPython.py
@@ -2,14 +2,18 @@
 @Author: Shital Bajait
 @Date: 2022-02-04 17:33:00
 @Last Modified by: Shital Bajait
-@Last Modified time: 2022-02-04 17:33:00
-@Title : Check employee is present or absent
+@Last Modified time: 2022-02-04 22:49:00
+@Title : Calculate Daily Employee Wage
 '''
 
 import random
 
+EMP_RATE_PER_HOUR = 20
 number = random.randint(0,1)
 if number == 1:
     print("Employee is present")
+    empHrs = 8
 else:
     print("Employee is absent")
+    empHrs = 0
+print("Daliy Wage is : ",empHrs * EMP_RATE_PER_HOUR)
