cf781661e6468977db9ed7c58ea279364964ed20
shital bramhe <shitalbramhe@gmail.com>
2022-02-05 17:18:48 +0530

    [Shital] add : Add part time Employee wage

--- EmployeeWageComputationUsingPython.py
+++ EmployeeWageComputationUsingPython.py
@@ -2,8 +2,8 @@
 @Author: Shital Bajait
 @Date: 2022-02-04 17:33:00
 @Last Modified by: Shital Bajait
-@Last Modified time: 2022-02-04 22:49:00
-@Title : Calculate Daily Employee Wage
+@Last Modified time: 2022-02-05 17:16:00
+@Title : Add part time Employee and Wage
 '''
 
 import random
@@ -11,9 +11,9 @@ import random
 EMP_RATE_PER_HOUR = 20
 number = random.randint(0,1)
 if number == 1:
-    print("Employee is present")
+    print("Employee is FullTime")
     empHrs = 8
 else:
-    print("Employee is absent")
-    empHrs = 0
+    print("Employee is PartTime")
+    empHrs = 4
 print("Daliy Wage is : ",empHrs * EMP_RATE_PER_HOUR)
