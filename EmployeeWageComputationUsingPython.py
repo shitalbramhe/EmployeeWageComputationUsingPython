f5875eaebc8ac164b00fc9939bca38d98cb7fc6d
shital bramhe <shitalbramhe@gmail.com>
2022-02-06 22:07:36 +0530

    [Shital] add : Employee wage for a month

--- EmployeeWageComputationUsingPython.py
+++ EmployeeWageComputationUsingPython.py
@@ -3,29 +3,29 @@
 @Date: 2022-02-04 17:33:00
 @Last Modified by: Shital Bajait
 @Last Modified time: 2022-02-05 17:22:00
-@Title : Employee Wage using switch case
+@Title : Wages for a month
 '''
 
 import random
 
-EMP_RATE_PER_HOUR = 20
-number = random.randint(0,2)
-def FullTime():
-    return 8
-def PartTime():
-    return 4
-def Absent():
-    return 0    
+EMP_RATE_PER_HOUR = 20 
+NUM_OF_WORKING_DAYS = 20
+empHrs = 0
+empWage = 0
+totalEmpWage=0
+day=0
+while day<NUM_OF_WORKING_DAYS:
+    num = random.randint(0,2)
+    if num == 0:
+        empHrs = 8
+    elif num == 1:
+        empHrs = 4
+    else:
+        empHrs = 0
+    
+    empWage = empHrs * EMP_RATE_PER_HOUR
+    totalEmpWage += empWage
+    day+=1
 
-switcher = {
-    0: Absent,
-    1: FullTime,
-    2: PartTime,
-}
-# Get the option from switcher dictionary
-option = switcher.get(number)
-
-empWage=option()*EMP_RATE_PER_HOUR
-
-#print employee wage
-print("Employee Wage:",empWage)
+print("Total Employee Wage : ",totalEmpWage)
+        
