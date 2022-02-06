d7a3f10b485f8e93651d7207b70f7df25bb03918
shital bramhe <shitalbramhe@gmail.com>
2022-02-06 08:48:48 +0530

    [Shital] add : Employee wage using switcher

--- EmployeeWageComputationUsingPython.py
+++ EmployeeWageComputationUsingPython.py
@@ -2,18 +2,30 @@
 @Author: Shital Bajait
 @Date: 2022-02-04 17:33:00
 @Last Modified by: Shital Bajait
-@Last Modified time: 2022-02-05 17:16:00
-@Title : Add part time Employee and Wage
+@Last Modified time: 2022-02-05 17:22:00
+@Title : Employee Wage using switch case
 '''
 
 import random
 
 EMP_RATE_PER_HOUR = 20
-number = random.randint(0,1)
-if number == 1:
-    print("Employee is FullTime")
-    empHrs = 8
-else:
-    print("Employee is PartTime")
-    empHrs = 4
-print("Daliy Wage is : ",empHrs * EMP_RATE_PER_HOUR)
+number = random.randint(0,2)
+def FullTime():
+    return 8
+def PartTime():
+    return 4
+def Absent():
+    return 0    
+
+switcher = {
+    0: Absent,
+    1: FullTime,
+    2: PartTime,
+}
+# Get the option from switcher dictionary
+option = switcher.get(number)
+
+empWage=option()*EMP_RATE_PER_HOUR
+
+#print employee wage
+print("Employee Wage:",empWage)
