e3bf0dd427180c0b5e0a0576f092f614ed27af0c
shital bramhe <shitalbramhe@gmail.com>
2022-02-07 21:02:08 +0530

    [Shital] refactor : Store Day and Daily Wage

--- EmployeeWageComputationUsingPython.py
+++ EmployeeWageComputationUsingPython.py
@@ -34,16 +34,16 @@ def total():
     totalEmpHrs = 0
     totalWorkingDays = 0
     totalEmpWage=0
-    empWage = 0
+    empWage = []
     while totalEmpHrs <= MAX_HRS_IN_MONTH and totalWorkingDays < NUM_OF_WORKING_DAYS:
-            totalWorkingDays+=1
+            
             number = random.randint(0,2)
             option = switcher.get(number)
             totalEmpHrs=totalEmpHrs+option()
-            empWage=option()*EMP_RATE_PER_HOUR
-            print("Daily wage : ",empWage)
-            totalEmpWage += empWage
+            empWage.append(option()*EMP_RATE_PER_HOUR)
+            print("Daily wage : ",empWage[totalWorkingDays])
+            totalWorkingDays+=1
     print("Total Employee Hours : ",totalEmpHrs)
-    print("Total Employee Wage : ",totalEmpWage)
+    print("Total Employee Wage : ",sum(empWage))
 #print employee wage
 total()
